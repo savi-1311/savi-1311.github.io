3fc0ce1c9aad669d9cd236e237e21174b0f52cb8
savi-1311 <shambhavishandilya01@gmail.com>
2024-05-26 14:44:37 +0530

    Modified Blogs

--- assets/js/medium-blogpost-widget.js
+++ assets/js/medium-blogpost-widget.js
@@ -33,27 +33,36 @@ template.innerHTML = `
   color: rgba(68, 68, 68, 0.8);
 }
 .medium-blogpost-articles {
+  padding: 10px;
   display: flex;
   flex-direction: column;
-  max-height: 300px;
+  align-items: center;
+  max-height: 500px;
   overflow: auto;
 }
 a {
   text-decoration: none;
+  color: inherit;
+  align-self: center;
+  align-items: center;
 }
 .medium-blogpost-single-article {
   padding: 10px;
+  width: 80%;
   border-bottom: 1px solid rgba(131, 131, 131, 0.2);
   display: flex;
   flex-direction: column;
   align-items: center;
   justify-content: center;
   text-align: center;
-  min-height: 100px;
+}
+.medium-blogpost-single-article img {
+  width: 80%;
 }
 
 .medium-blogpost-single-article h3 {
   margin: 0;
+  text-align: center;
 }
 .medium-blogpost-single-article p {
   margin: 0;
@@ -88,41 +97,43 @@ a {
 
 `
 class MediumBlogpost extends HTMLElement {
-    constructor() {
-        super()
-        this._shadowRoot = this.attachShadow({ mode: 'open' });
-        this._shadowRoot.appendChild(template.content.cloneNode(true));
-    }
-    async fetchPosts(username) {
-        const response = await fetch(`https://api.rss2json.com/v1/api.json?rss_url=https://medium.com/feed/@${username}`)
-        const data = await response.json()
-        return data
-    }
-    connectedCallback(){
-        this.username = this.getAttribute("username")
-        this.render()
-    }
-    renderUser(data){
-        this._shadowRoot.querySelector('.medium-blogpost-author-img').innerHTML = `<img src="${data.feed.image}" alt="${this.name}"/>`
-        this._shadowRoot.querySelector('.medium-blogpost-author-info').innerHTML = `<h3>${data.items[0].author}</h3><p>@${this.username}</p><a href="https://medium.com/@${this.username}" target="_blank"><button class="medium-follow-button">Follow</button></a>`
-    }
+  constructor() {
+    super()
+    this._shadowRoot = this.attachShadow({ mode: 'open' });
+    this._shadowRoot.appendChild(template.content.cloneNode(true));
+  }
+  async fetchPosts(username) {
+    const response = await fetch(`https://api.rss2json.com/v1/api.json?rss_url=https://medium.com/feed/@${username}`)
+    const data = await response.json()
+    return data
+  }
+  connectedCallback() {
+    this.username = this.getAttribute("username")
+    this.render()
+  }
+  renderUser(data) {
+    this._shadowRoot.querySelector('.medium-blogpost-author-img').innerHTML = `<img src="${data.feed.image}" alt="${this.name}"/>`
+    this._shadowRoot.querySelector('.medium-blogpost-author-info').innerHTML = `<h3>${data.items[0].author}</h3><p>@${this.username}</p><a href="https://medium.com/@${this.username}" target="_blank"><button class="medium-follow-button">Follow</button></a>`
+  }
 
-    renderArticles(data=[]){
-        data.forEach(data =>{
-            this._shadowRoot.querySelector('.medium-blogpost-articles').innerHTML +=`<a style="background-size: contain;"class="medium-blogpost-single-article" href="${data.link}" target="_blank"><h3>${data.title}</h3><p>${this.parseDate(data.pubDate)}</p></a>`
-        })
-    }
-    parseDate(date) {
-        const IsoStringToDate = new Date(date);
-        const parsedDate = IsoStringToDate.toUTCString().slice(5).slice(0, -13);
-        return parsedDate;
-    };
+  renderArticles(data = []) {
+    data.forEach(data => {
+      var firstImage = data.description.match(/<img[^>]+src="([^">]+)"/)[1];
+      this._shadowRoot.querySelector('.medium-blogpost-articles').innerHTML += `<a class="medium-blogpost-single-article" href="${data.link}" target="_blank"><img src='${firstImage}'/></a>`
+      this._shadowRoot.querySelector('.medium-blogpost-articles').innerHTML += `<a href=href="${data.link}"><h3>${data.title}</h3></a><p>${this.parseDate(data.pubDate)}</p>`
+    })
+  }
+  parseDate(date) {
+    const IsoStringToDate = new Date(date);
+    const parsedDate = IsoStringToDate.toUTCString().slice(5).slice(0, -13);
+    return parsedDate;
+  };
 
-    async render(){
-        const data =  await this.fetchPosts(this.username)
-        this.renderUser(data)
-        this.renderArticles(data.items)
-    }
+  async render() {
+    const data = await this.fetchPosts(this.username)
+    this.renderUser(data)
+    this.renderArticles(data.items)
+  }
 }
 
 customElements.define('medium-blogpost', MediumBlogpost)
